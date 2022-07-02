541bf3860fd7b8070464eede995ebc65294be88e
Sunshine Moua <sunshinemoua@Sunshines-MacBook-Pro.local>
2022-07-01 23:34:52 -0700

    updated cart functionality

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
@@ -10,10 +11,21 @@ const Cart = (props) => {
 
   const hasItems = cartCtx.items.length > 0;
 
+  const cartItemRemoveHandler = (id) => {};
+
+  const cartItemAddHandler = (item) => {};
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
-        <li>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          quantity={item.quantity}
+          cartItemRemove={cartItemRemoveHandler.bind(null, item.id)}
+          cartItemAddHandler={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
